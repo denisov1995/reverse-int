4d74aeb2aeea0c7c3389080c11e08502b820b9ab
denisov1995 <bysama1995@gmail.com>
2021-03-07 15:35:43 +0300

    fit: end task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
-}
+    arr=String(n)
+    a = arr.split('')
+    for (let i = 0; i < a.length; i++) {
+        if (a[0]=== '-') {
+          delete a[0]
+        }  
+    }
+    result = a.reverse().join('')
+    return (result)
+  }
+
